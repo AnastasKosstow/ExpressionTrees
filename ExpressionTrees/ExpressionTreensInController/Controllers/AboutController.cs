bbebcdeef19a6c1ca1d9d2c469d7fbf4aa8f1edd
Anastas Kosstow <anastas.kosstow@protonmail.com>
2020-03-14 23:26:38 +0200

    Expression Trees In Controller

---
+++ ExpressionTrees/ExpressionTreensInController/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ExpressionTreesInController.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ExpressionTreesInController.Infrastructure;
+
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.RedirectTo<HomeController>(c => c.Index());
+        }
+    }
+}
---
+++ ExpressionTrees/ExpressionTreensInController/Infrastructure/ControllerExtensions.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ExpressionTreesInController.Infrastructure
+{
+    using System;
+    using System.Linq.Expressions;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public static class ControllerExtensions
+    {
+        public static IActionResult RedirectTo<T>(this Controller controller, Expression<Action<T>> redirect)
+            where T : Controller
+        {
+            if (redirect.Body.NodeType != ExpressionType.Call)
+                throw new InvalidOperationException($"{redirect.Body}");
+
+            return GetRedirectFromExpression<T>(controller, redirect);
+        }
+
+
+        private static IActionResult GetRedirectFromExpression<TController>(Controller controller, LambdaExpression redirect)
+        {
+            MethodCallExpression methodCallExpression = (MethodCallExpression)redirect.Body;
+
+            string actionName = GetActionName(methodCallExpression);
+            string controllerName = GetControllerName<TController>();
+
+            return controller.RedirectToAction(actionName, controllerName);
+        }
+
+
+        private static string GetActionName(MethodCallExpression methodCallExpression)
+            =>
+            methodCallExpression.Method.Name;
+
+
+        private static string GetControllerName<TController>()
+            => 
+            typeof(TController).Name.Replace(nameof(Controller), string.Empty);
+    }
+}
